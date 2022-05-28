b40498ea740311d65be6d399d48802df560d39ee
Sayed ELGhanam <sayed082961@feng.bu.edu.eg>
2022-05-28 22:27:52 +0200

    create context

---
+++ src/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext:ICatalogContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var client = new MongoClient(configuration["DatabaseStrings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseStrings:DatabaseName"]);
+
+            Products = database.GetCollection<Product>(nameof(Product));
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
