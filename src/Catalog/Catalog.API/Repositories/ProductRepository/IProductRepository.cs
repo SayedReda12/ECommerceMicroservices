72a93ddd06e3a48e37e5f919f99bcab718418285
Sayed ELGhanam <sayed082961@feng.bu.edu.eg>
2022-05-31 00:55:27 +0200

    create product repository

---
+++ src/Catalog/Catalog.API/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(string id);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<IEnumerable<Product>> GetProductsByCategory(string categoryName);
+
+        Task<Product> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string productId);
+
+    }
+}
---
+++ src/Catalog/Catalog.API/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories.ProductRepository
+{
+    public class ProductRepository:IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext;
+        }
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _catalogContext.Products.Find(a => true).ToListAsync();
+        }
+
+        public async Task<Product> GetProductById(string id)
+        {
+            return await _catalogContext.Products.Find(a => a.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByName(string name)
+        {
+            var filter = Builders<Product>.Filter.Eq(a => a.Name, name);
+            return await _catalogContext.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategory(string categoryName)
+        {
+            var filter = Builders<Product>.Filter.Eq(a => a.Category, categoryName);
+
+            return await _catalogContext.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<Product> CreateProduct(Product product)
+        {
+           await _catalogContext.Products.InsertOneAsync(product);
+
+           return product;
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updateResult = await _catalogContext.Products.ReplaceOneAsync(a => a.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteProduct(string productId)
+        {
+            var deletedResult = await _catalogContext.Products.DeleteOneAsync(a => a.Id == productId);
+            return deletedResult.IsAcknowledged && deletedResult.DeletedCount > 0;
+        }
+    }
+}
